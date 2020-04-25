d6d40530c0d4d266403f2244f8c146bca381a18f
WerewolfDevLearn <werewolfdevlearn@gmail.com>
2020-04-25 22:31:51 +0300

    all tasks are done

--- script/task-6.js
+++ script/task-6.js
@@ -1,21 +1,24 @@
 let input;
 let cncl;
 let total = 0;
-let numbers = [];
-const logItems = function(array) {
-  for (let i = 0; i < array.length; i += 1) {
-    console.log(`${i + 1} - ${array[i]}`);
-  }
-};
-
+const numbers = [];
 do {
   input = prompt('Введите число:');
   cncl = input;
+  input = Number(input);
   if (Number.isNaN(input) === true) {
     alert('Вы ввели не число. Попробуйте еще раз');
+  } else if (cncl === '') {
+    input = '';
+    alert('Вы ничего не ввели.');
   } else {
     numbers.push(input);
-    console.log(numbers);
   }
 } while (cncl !== null);
+numbers.pop();
+
+for (let i = 0; i < numbers.length; i += 1) {
+  console.log(numbers[i]);
+  total += total + numbers[i];
+}
 alert(`Общая сумма чисел равна ${total}`);
