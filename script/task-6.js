af1912f2b360b7d83635cd51b3bbc7a63d0eb2f3
WerewolfDevLearn <werewolfdevlearn@gmail.com>
2020-04-26 19:55:26 +0300

    fix mistakes in task-7

--- script/task-6.js
+++ script/task-6.js
@@ -2,6 +2,7 @@ let input;
 let cncl;
 let total = 0;
 const numbers = [];
+
 do {
   input = prompt('Введите число:');
   cncl = input;
@@ -11,14 +12,23 @@ do {
   } else if (cncl === '') {
     input = '';
     alert('Вы ничего не ввели.');
+  } else if (cncl === null) {
+    input = '';
+    break;
   } else {
     numbers.push(input);
   }
+  console.log(input);
+  console.log(numbers);
 } while (cncl !== null);
-numbers.pop();
-
-for (let i = 0; i < numbers.length; i += 1) {
-  console.log(numbers[i]);
-  total += numbers[i];
+// numbers.pop();
+console.log(numbers);
+if (numbers.length === 0) {
+  alert('Вы ничего не ввели. Нечего считать');
+} else {
+  for (let i = 0; i < numbers.length; i += 1) {
+    console.log(numbers[i]);
+    total += numbers[i];
+  }
+  alert(`Общая сумма чисел равна ${total}`);
 }
-alert(`Общая сумма чисел равна ${total}`);
